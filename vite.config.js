0bc5d733acdfb1414e1138c6ac5874d1be8a869b
cookiecreates <devwratjk09@gmail.com>
2025-07-21 10:26:42 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/cookie-about-me/', // 👈 this is crucial for GitHub Pages
-});
